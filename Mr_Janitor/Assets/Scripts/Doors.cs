5fe197c0a6b6a264eedcfd8cff213420e2e982da
Riley Meats <66439825+Riley-Meats@users.noreply.github.com>
2022-01-14 08:18:48 -0800

    doors almost work

--- Mr_Janitor/Assets/Scripts/Doors.cs
+++ Mr_Janitor/Assets/Scripts/Doors.cs
@@ -7,19 +7,50 @@ public class Doors : MonoBehaviour
 {
     public float tpX = 20f;
     public float tpY = -5f;
+    int i;
+
+    public Collider2D boundsTrigger;
+
+    List<GameObject> internalGameObjects = new List<GameObject>();
 
     Collision2D collision;
 
     Animator animator;
 
-    void Start()
+    public GameObject[] items;
+
+    public void Start()
     {
         animator = GetComponent<Animator>();
+
+        GameObject[] itemsInsideCollider = GetItemsInsideCollider();
+        Debug.Log("There are " + itemsInsideCollider.Length + " items in the collider!");
+
+    }
+
+    public GameObject[] GetItemsInsideCollider()
+    {
+        
+
+        for (i = 0; i < items.Length; i++)
+        {
+            if (boundsTrigger.bounds.Contains(items[i].transform.position))
+            {
+                internalGameObjects.Add(items[i]);
+            }
+        }
+
+        for (i = 0; i > items.Length; i--)
+        {
+            
+        }
+
+        return internalGameObjects.ToArray();
     }
 
     void Update()
     {
-        
+
     }
 
     public void DoorHit()
@@ -29,8 +60,13 @@ public class Doors : MonoBehaviour
 
     public void OnCollisionEnter2D(Collision2D other)
     {
-        //GetComponent<TPBlackPlay>().Play(animator.SetTrigger("TPScreen"));
-        collision = other;
-        Invoke("DoorHit", 2);
+
+        if (items.Length == 0)
+        {
+            //GetComponent<TPBlackPlay>().Play(animator.SetTrigger("TPScreen"));
+            collision = other;
+            Invoke("DoorHit", 2);
+        }
+
     }
 }

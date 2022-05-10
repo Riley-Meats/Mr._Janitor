beb82ae777d2ccfcb896472752938511da77959e
Riley Meats <66439825+Riley-Meats@users.noreply.github.com>
2022-05-10 08:55:59 -0700

    random spawn of the platform works and random spawn of the 3 heath ups works as well but i can't collect the health up

--- Mr_Janitor/Assets/Scripts/SpawnPlatform.cs
+++ Mr_Janitor/Assets/Scripts/SpawnPlatform.cs
@@ -4,28 +4,52 @@ using UnityEngine;
 
 public class SpawnPlatform : MonoBehaviour
 {
-    float percent;
+    public float percent = 1.0f;
+    float hPercent;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Vector2 spawn;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject platform;
+    public GameObject healthUp2;
+    public GameObject healthUp4;
+    public GameObject healthUp6;
 
     void Chance()
     {
         percent = Random.value;
+
+        if (percent <= .25f)
+        {
+            hPercent = Random.value;
+        }
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
         Chance();
         Debug.Log(percent);
+
+        if (percent <= .25f)
+        {
+            Instantiate(platform);
+        }
+
+        if (percent <= .25f)
+        {
+            Debug.Log("your percent was " + hPercent);
+
+            if (hPercent >= 0.00001f && hPercent < .50f)
+            {
+                Instantiate(healthUp2, spawn, Quaternion.identity);
+            }
+            else if (hPercent > .50f && hPercent <= .80f)
+            {
+                Instantiate(healthUp4, spawn, Quaternion.identity);
+            }
+            else if (hPercent > .80 && hPercent <= 1.0f)
+            {
+                Instantiate(healthUp6, spawn, Quaternion.identity);
+            }
+        }
     } 
 }
